92bb562eac2681b85aef75a517ee73a70daebe96
alejox <alejoxdrums@gmail.com>
2024-02-05 21:35:00 -0500

    add changes

--- src/components/ScrollTop/ScrollTop.jsx
+++ src/components/ScrollTop/ScrollTop.jsx
@@ -1,15 +1,37 @@
+import React, { useState, useEffect } from "react";
 import "./ScrollTop.scss";
-import arrow from '../icons/flecha-arriba.svg'
+import arrow from "../icons/flecha-arriba.svg";
 
 const ScrollTop = () => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 1000) {
+        setIsVisible(true);
+      } else {
+        setIsVisible(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   const scrollToTop = () => {
     window.scrollTo({ top: 0, behavior: "smooth" });
   };
+
   return (
     <div className="scroll">
-      <button className="btnTop " onClick={scrollToTop}>
-        <img width={30} src={arrow} alt="" />
-      </button>
+      {isVisible && (
+        <button className="btnTop" onClick={scrollToTop}>
+          <img width={20} src={arrow} alt="" />
+        </button>
+      )}
     </div>
   );
 };
